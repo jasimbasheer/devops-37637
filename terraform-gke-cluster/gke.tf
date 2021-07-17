7acb6520dd743ec4398f777dbb7c9583e2189490
Jasim Basheer <jasimpp1@gmail.com>
2021-07-17 17:31:12 +0530

    wordpress_with_kustomize

--- terraform-gke-cluster/gke.tf
+++ terraform-gke-cluster/gke.tf
@@ -9,7 +9,7 @@ variable "gke_password" {
 }
 
 variable "gke_num_nodes" {
-  default     = 2
+  default     = 1
   description = "number of gke nodes"
 }
 
@@ -47,6 +47,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # preemptible  = true
     machine_type = "n1-standard-1"
+    image_type = "ubuntu"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
